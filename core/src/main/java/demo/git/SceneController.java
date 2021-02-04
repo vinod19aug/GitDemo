c21063f418283fa7a7c94b1167a147fcd1f726f9
Vinod <vinod.devxam@gmail.com>
2021-02-04 13:28:58 +0530

    Source Demo

--- core/src/main/java/demo/git/SceneController.java
+++ core/src/main/java/demo/git/SceneController.java
@@ -1,7 +1,12 @@
 package demo.git;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
-public class SceneController extends ApplicationAdapter {
+public class SceneController extends Game {
+    @Override
+    public void create() {
+
+    }
 }
