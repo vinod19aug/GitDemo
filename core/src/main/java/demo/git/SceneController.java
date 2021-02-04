35770f8c6b307a6b9cc8d9fe84527f144f7eebae
Vinod <vinod.devxam@gmail.com>
2021-02-04 13:14:17 +0530

    Source Demo

---
+++ core/src/main/java/demo/git/SceneController.java
@@ -0,0 +1,7 @@
+package demo.git;
+
+import com.badlogic.gdx.ApplicationAdapter;
+
+/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
+public class SceneController extends ApplicationAdapter {
+}
