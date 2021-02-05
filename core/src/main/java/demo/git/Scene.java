205080f70cad6a2c79d73495bf377834e9ef30dc
Vinod <vinod.devxam@gmail.com>
2021-02-05 18:12:47 +0530

    Stage initialization in Scene Model Class

--- core/src/main/java/demo/git/Scene.java
+++ core/src/main/java/demo/git/Scene.java
@@ -1,16 +1,26 @@
 package demo.git;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputMultiplexer;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
-public class Scene implements Screen {
+public class Scene implements Screen , InputProcessor {
     public SceneController controller;
+    public Stage stage;
+    public InputMultiplexer inputMultiplexer;
     public Scene(SceneController controller){
         this.controller=controller;
+        this.stage=new Stage(new ScreenViewport());
+        this.inputMultiplexer=new InputMultiplexer(stage,this);
     }
 
     @Override
     public void show() {
-
+        Gdx.input.setInputProcessor(this.inputMultiplexer);
     }
 
     @Override
@@ -41,5 +51,45 @@ public class Scene implements Screen {
     @Override
     public void dispose() {
 
+    }
+
+    @Override
+    public boolean keyDown(int keycode) {
+        return false;
+    }
+
+    @Override
+    public boolean keyUp(int keycode) {
+        return false;
+    }
+
+    @Override
+    public boolean keyTyped(char character) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDragged(int screenX, int screenY, int pointer) {
+        return false;
+    }
+
+    @Override
+    public boolean mouseMoved(int screenX, int screenY) {
+        return false;
+    }
+
+    @Override
+    public boolean scrolled(float amountX, float amountY) {
+        return false;
     }
 }
