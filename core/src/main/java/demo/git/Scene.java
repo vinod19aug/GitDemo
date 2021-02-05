931df401b1c1e3a5ea15aaf1bd1835c777798cbc
Vinod <vinod.devxam@gmail.com>
2021-02-05 18:08:37 +0530

    Revert "Scene Model Class"
    This reverts commit 92c4c6a3

--- core/src/main/java/demo/git/Scene.java
+++ core/src/main/java/demo/git/Scene.java
@@ -1,30 +1,16 @@
 package demo.git;
 
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.InputMultiplexer;
-import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.scenes.scene2d.Actor;
-import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
-public class Scene implements Screen, InputProcessor {
+public class Scene implements Screen {
     public SceneController controller;
-    public Stage stage;
-    public InputMultiplexer inputMultiplexer;
     public Scene(SceneController controller){
         this.controller=controller;
-        this.stage=new Stage(new ScreenViewport());
-        this.inputMultiplexer=new InputMultiplexer(stage,this);
-    }
-
-    public void addActor(Actor actor){
-        this.stage.addActor(actor);
     }
 
     @Override
     public void show() {
-        Gdx.input.setInputProcessor(inputMultiplexer);
+
     }
 
     @Override
@@ -55,45 +41,5 @@ public class Scene implements Screen, InputProcessor {
     @Override
     public void dispose() {
 
-    }
-
-    @Override
-    public boolean keyDown(int keycode) {
-        return false;
-    }
-
-    @Override
-    public boolean keyUp(int keycode) {
-        return false;
-    }
-
-    @Override
-    public boolean keyTyped(char character) {
-        return false;
-    }
-
-    @Override
-    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-        return false;
-    }
-
-    @Override
-    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
-        return false;
-    }
-
-    @Override
-    public boolean touchDragged(int screenX, int screenY, int pointer) {
-        return false;
-    }
-
-    @Override
-    public boolean mouseMoved(int screenX, int screenY) {
-        return false;
-    }
-
-    @Override
-    public boolean scrolled(float amountX, float amountY) {
-        return false;
     }
 }
