92c4c6a3f7619d79c2f5c84d3e1e7ff4e03f5ef4
Vinod <vinod.devxam@gmail.com>
2021-02-05 15:08:01 +0530

    Scene Model Class

---
+++ core/src/main/java/demo/git/Scene.java
@@ -0,0 +1,45 @@
+package demo.git;
+
+import com.badlogic.gdx.Screen;
+
+public class Scene implements Screen {
+    public SceneController controller;
+    public Scene(SceneController controller){
+        this.controller=controller;
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
